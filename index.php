75e9984535f8ce5405765306ed64b748de288a0f
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2012-02-01 10:22:51 -0800

    Add index.php file with meta-data for the plugin

---
+++ index.php
@@ -0,0 +1,15 @@
+
+
+<?php
+
+/*
+Plugin Name: Placeling
+Plugin URI: https://www.placeling.com
+Description: Tag your posts with location data from Placeling!
+Version: 0.1
+Author: Placeling (Internet Services) Inc.
+Author URI: https://www.placeling.com
+*/
+
+?>
+
