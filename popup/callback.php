1421244abd35898b389f7946a1fbde4e82123871
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2012-02-22 21:38:29 -0800

    cleanup how credentials are received

--- popup/callback.php
+++ popup/callback.php
@@ -48,24 +48,9 @@
     // Example Google API Access:
     // This will build a link to an RSS feed of the users calendars.
     $oauthObject->reset();
-    /* $result = $oauthObject->sign(array(
-       'path'      =>'http://www.google.com/calendar/feeds/default/allcalendars/full',
-        'parameters'=> array('orderby' => 'starttime'),
-        'signatures'=> $signatures));
-	*/
-    // Instead of going to the list, I will just print the link along with the 
-    // access token and secret, so we can play with it in the sandbox:
-    // http://googlecodesamples.com/oauth_playground/
-    //
-    //curl_setopt($ch, CURLOPT_URL, $result['signed_url']);
-    curl_close($ch);
 
 	update_option('placeling_access_token', $access_token);
 	update_option('placeling_access_secret', $access_token_secret);
 	
-	echo "<p>Access Token: "+ get_option('placeling_access_token') + "<BR>
-                  Token Secret:"+ get_option('placeling_access_secret') +"</p>";
-	
-	//header( 'Location:index.php' ) ;	
-
+	header( 'Location:index.php' ) ;	
 ?>
