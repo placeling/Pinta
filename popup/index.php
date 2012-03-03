3e7ecbb9e04fd5dd321bb02559eda19b918cbfc5
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2012-03-03 11:18:10 -0800

    remove reference to the deleted pinta javascript file

--- popup/index.php
+++ popup/index.php
@@ -119,7 +119,7 @@
 		<link rel="stylesheet" href="../css/jquery-ui-1.8.18.custom.css" type="text/css"/>
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
-		<script type="text/javascript" src="../js/pinta.js"></script>
+		
 		<style type="text/css">
 			.ui-progressbar-value { background-image: url(../css/images/pbar-ani.gif); }
 		</style>
