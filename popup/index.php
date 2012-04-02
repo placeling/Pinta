1043cf656972f254373b7050e5fa01c11239aa44
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2012-04-02 15:56:47 -0700

    PIN-26, make more IE-friendly javascript

--- popup/index.php
+++ popup/index.php
@@ -168,23 +168,24 @@
 				
 				showRecentlyBookmarked();
 				
-				$.xhrPool = [];
+				$.xhrPool = new Array();
 				$.xhrPool.abortAll = function() {
 				    $(this).each(function(idx, jqXHR) {
 					jqXHR.abort();
 				    });
 				    $.xhrPool = [];
 				};
-				
+
+
 				$.ajaxSetup({
 				    beforeSend: function(jqXHR) {
-					$.xhrPool.push(jqXHR);
+					    //$.xhrPool.push(jqXHR);
 				    },
 				    complete: function(jqXHR) {
-					var index = $.xhrPool.indexOf(jqXHR);
-					if (index > -1) {
-					    $.xhrPool.splice(index, 1);
-					}
+					    var index = jQuery.inArray( jqXHR, $.xhrPool );
+					    if (index > -1) {
+					        $.xhrPool.splice(index, 1);
+					    }
 				    }
 				});
 				
