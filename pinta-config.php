9c00e199034ffb9e6eac2aa8f89dfe1d2cda363c
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2012-04-16 15:22:21 -0700

    set the update to a day

--- pinta-config.php
+++ pinta-config.php
@@ -3,5 +3,5 @@
     $WEB_HOSTNAME= "http://www.placeling.com";
     $SIGNATURES = array( 'consumer_key'     => 'IR3hVvWRYBp1ah3PJUiPirgFzKlMHTeujbORNzAK',
 				'shared_secret'    => 'PqsYkO2smE7gkz9txhzN0bHoPMtDLfp73kIc3RSY');
-    $RELOAD_INTERVAL = 60*60*24*10;
+    $RELOAD_INTERVAL = 60*60*24*1;
 ?>
