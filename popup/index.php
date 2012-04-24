234a760594e7e758bde905753c5f62fb50a50d17
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2012-04-24 11:15:20 -0700

    don't want routes, just locations

--- popup/index.php
+++ popup/index.php
@@ -211,7 +211,7 @@
 								data: {
 									input: text,
 									location: lat + "," + lng,
-									types: "establishment|geocode"
+									types: "establishment|(regions)"
 								},
 								success: function( data ) {
 									$("ul#recent_places li").remove();
