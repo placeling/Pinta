27305333193ed85dd8da170aed7d7ebd722b2123
Ian MacKinnon <imackinn@alumni.uwaterloo.ca>
2012-04-30 12:04:29 -0700

    point to https and increase refresh interval for performance reasons

--- pinta-config.php
+++ pinta-config.php
@@ -1,7 +1,7 @@
 <?php
     $SERVICE_HOSTNAME= "https://api.placeling.com";
-    $WEB_HOSTNAME= "http://www.placeling.com";
+    $WEB_HOSTNAME= "https://www.placeling.com";
     $SIGNATURES = array( 'consumer_key'     => 'IR3hVvWRYBp1ah3PJUiPirgFzKlMHTeujbORNzAK',
 				'shared_secret'    => 'PqsYkO2smE7gkz9txhzN0bHoPMtDLfp73kIc3RSY');
-    $RELOAD_INTERVAL = 60*60*24*1;
+    $RELOAD_INTERVAL = 60*60*24*3;
 ?>
