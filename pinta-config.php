a94fd4e260ac8f03e31af4b76bc2428847656373
imackinn <imackinn@alumni.uwaterloo.ca>
2013-01-25 16:16:32 -0800

    2.1 release with zoom control in settings page

--- pinta-config.php
+++ pinta-config.php
@@ -1,6 +1,6 @@
 <?php
-    $PLACELING_SERVICE_HOSTNAME= "https://api.placeling.com";
-    $PLACELING_WEB_HOSTNAME= "https://www.placeling.com";
+    $PLACELING_SERVICE_HOSTNAME= "http://localhost:3000";
+    $PLACELING_WEB_HOSTNAME= "http://localhost:3000";
     $PLACELING_SIGNATURES = array( 'consumer_key'     => 'IR3hVvWRYBp1ah3PJUiPirgFzKlMHTeujbORNzAK',
 				'shared_secret'    => 'PqsYkO2smE7gkz9txhzN0bHoPMtDLfp73kIc3RSY');
     $PLACELING_RELOAD_INTERVAL = 60*60*24*3;
