1aa387b15d06867c7c9373eee4a85be4dbb1a866
imackinn <imackinn@alumni.uwaterloo.ca>
2013-01-30 14:00:20 -0800

    kick up refresh interval to a week

--- pinta-config.php
+++ pinta-config.php
@@ -7,5 +7,5 @@
     $PLACELING_SIGNATURES = array( 'consumer_key'     => 'IR3hVvWRYBp1ah3PJUiPirgFzKlMHTeujbORNzAK',
 				'shared_secret'    => 'PqsYkO2smE7gkz9txhzN0bHoPMtDLfp73kIc3RSY');
     global $PLACELING_RELOAD_INTERVAL;
-    $PLACELING_RELOAD_INTERVAL = 60*60*24*3;
+    $PLACELING_RELOAD_INTERVAL = 60*60*24*7;
 ?>
